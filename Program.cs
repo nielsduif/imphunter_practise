403e7a915e83ed1f1e79a16a84b1a193f7e91cfb
Remco van Swieten <r.van.swieten@hva.nl>
2021-03-18 14:02:09 +0100

    Tentamen start

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ImpHunter
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new ImpHunter())
+                game.Run();
+        }
+    }
+}
