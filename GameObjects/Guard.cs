12f82b5895ed1dd292dd6146b70f8be5f445b9c1
nielsduif <niels-duivenvoorden@hotmail.com>
2021-06-21 22:14:27 +0200

    guards

---
+++ GameObjects/Guard.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImpHunter2021.GameObjects
+{
+    class Guard : SpriteGameObject
+    {
+        Vector2 startPos;
+        int speedHor = 50, speedVer = 0;
+        public Guard(Vector2 _startPos, string _assetName = "spr_guard") : base(_assetName)
+        {
+            startPos = _startPos;
+            Reset();
+        }
+
+        public override void Reset()
+        {
+            base.Reset();
+            position = startPos;
+            velocity = new Vector2(speedHor, speedVer);
+        }
+
+        public void Bounce()
+        {
+            velocity.X *= -1;
+        }
+    }
+}
--- GameStates/PlayingState.cs
+++ GameStates/PlayingState.cs
@@ -1,4 +1,5 @@
 ﻿using ImpHunter2021.GameObjects;
+using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,16 +11,52 @@ namespace ImpHunter2021.GameStates
         Crosshair ch;
         GameObjectList imps;
         int impAmount = 3;
+        GameObjectList guards;
+        int guardAmount = 3;
+        SpriteGameObject walls;
         public PlayingState()
         {
             Add(new SpriteGameObject("spr_background"));
+
+            Add(walls = new SpriteGameObject("spr_towers"));
+
             Add(ch = new Crosshair());
+
             imps = new GameObjectList();
             for (int i = 0; i < impAmount; i++)
             {
                 imps.Add(new Imps());
             }
             Add(imps);
+
+            guards = new GameObjectList();
+            for (int i = 0; i < guardAmount; i++)
+            {
+                guards.Add(new Guard(new Vector2(170 + 150 * i, 490)));
+            }
+            Add(guards);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            for (int i = guards.Children.Count - 1; i >= 0; i--)
+            {
+                Guard g = (guards.Children[i] as Guard);
+                if (g.CollidesWith(walls))
+                {
+                    g.Bounce();
+                }
+
+                for (int x = guards.Children.Count - 1; x >= 0; x--)
+                {
+                    Guard og = (guards.Children[x] as Guard);
+                    if (g.CollidesWith(og))
+                    {
+                        g.Bounce();
+                    }
+                }
+            }
         }
     }
 }
