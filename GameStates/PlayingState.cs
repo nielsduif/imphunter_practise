cc2ab9dcb368e82f8bf829d8a953ae1025fd19c2
nielsduif <niels-duivenvoorden@hotmail.com>
2021-06-21 21:40:08 +0200

    content, playingstate

---
+++ GameStates/PlayingState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImpHunter2021.GameStates
+{
+    class PlayingState : GameObjectList
+    {
+        public PlayingState()
+        {
+            Add(new SpriteGameObject("spr_background"));
+        }
+    }
+}
